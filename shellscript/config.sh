b005b8f3924c1d111750491485502792eef47882
eduarda <eduardamelgarejo060@gmail.com>
2025-04-14 08:15:55 -0400

    feat: shell script basico

---
+++ shellscript/config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+apt -y install nano git
+apt update
+apt -y upgrade
+git clone https://github.com/dudamel007/progredes
+cd progredes
+ls
+
---
+++ shellscript/variaveis.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+clear
+nome="eduarda"
+idade=20
+echo ola, $nome de idade $idade!
+
